8927627e3ece7d41199fbdc5321a083ba588bf20
Ajinkya <42488697+nanawareajinkya@users.noreply.github.com>
2020-12-27 16:53:52 +0530

    File creates

---
+++ build/precache-manifest.5ab738f9f8a8529f830cdf2a3e3ab682.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1174a39e92cb0858957bc46235136df0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "411a6311505ffbc04ef0",
+    "url": "/static/css/main.5eb627e4.chunk.css"
+  },
+  {
+    "revision": "3aa1c3d1b753a2dada3b",
+    "url": "/static/js/2.6141d88c.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.6141d88c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "411a6311505ffbc04ef0",
+    "url": "/static/js/main.085b0cf0.chunk.js"
+  },
+  {
+    "revision": "9d9a4c99211d98368a0d",
+    "url": "/static/js/runtime-main.7b3982eb.js"
+  }
+]);
